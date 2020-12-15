55787ddb43d41121d557817eb747e35c0b1b1021
tbisho <tess.bishop8@gmail.com>
2020-12-15 12:50:24 -0700

    pulls info from year totals endpoint and displays on profile

--- routes/profile.js
+++ routes/profile.js
@@ -10,36 +10,62 @@ var fs = require('fs');
 
 // testing route
 
-router.get('/', (req, res) => {
-    let activities = fs.readFileSync('./activities.json')
-    let activityData = JSON.parse(activities)
-    let yearTotals = fs.readFileSync('./yearToDate.json')
-    let profileInfo = fs.readFileSync('./profileInfo.json')
-    let profileData = JSON.parse(profileInfo)
+// router.get('/', (req, res) => {
+//     let activities = fs.readFileSync('./activities.json')
+//     let activityData = JSON.parse(activities)
+//     let yearTotals = fs.readFileSync('./yearToDate.json')
+//     let profileInfo = fs.readFileSync('./profileInfo.json')
+//     let profileData = JSON.parse(profileInfo)
 
 
 
 
-    yearData = JSON.parse(yearTotals)
-    console.log('first activity', activityData[0].name)
-    console.log(yearData.ytd_ride_totals)
-    console.log('profile info:', profileData.profile_medium)
-    res.render('profile', { activity: activityData[0], yearTotals: yearData, profileInfo: profileData })
-  });
+//     yearData = JSON.parse(yearTotals)
+//     console.log('first activity', activityData[0].name)
+//     console.log(yearData.ytd_ride_totals)
+//     console.log('profile info:', profileData.profile_medium)
+//     res.render('profile', { activity: activityData[0], yearTotals: yearData, profileInfo: profileData })
+//   });
 
   // {yearTotals: yearData}
 
 
 
-// TESTING API CONNECTION
-// router.get('/', function(req,res) {
-//   // get athlete stats
-//   const athleteUrl = "https://www.strava.com/api/v3/athlete/activities?before=&after=&page=&per_page=?access_token=18cba95a3d1772ebca4148dd5bee0b471085c62d "
-//     axios.get(athleteUrl)
-//     .then( function(apiResponse) {
-//     console.log(apiResponse)
-//     res.render('profile')
-//   })
+
+router.get('/', function(req,res) {
+
+// get athlete stats
+console.log('hitting route?')
+const statsUrl = "https://www.strava.com/api/v3/athletes/47183122/stats"
+
+axios.get(statsUrl,
+  {
+    headers: {
+      "Authorization": `Bearer ${req.user.access_token}`
+    }
+  }).then(function(apiResponse) {
+    // console.log('FIRST', apiResponse)
+    res.render('profile', { yearTotals: apiResponse.data})
+  })
+});
+  
+//   axios.all([
+//     axios.get(athleteUrl,
+//   {
+//     headers: {
+//       "Authorization": `Bearer ${req.user.access_token}`
+//     }
+//   }),
+//   axios.get('https://www.strava.com/api/v3/athletes')
+// ]).then(axios.spread((...responses) => {
+//   console.log(...reponses)
 // })
+
+// .then(axios.spread((apiResponse1, apiResponse2) => {
+//     // console.log(apiResponse1, apiResponse2)
+//     res.render('profile', { yearTotals: apiResponse1.data })
+
+// }))
+
 
 module.exports = router;
