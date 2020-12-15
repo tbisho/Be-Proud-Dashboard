c8f010b8bc3bc71fd5449b240066d76effb4fcd9
tbisho <tess.bishop8@gmail.com>
2020-12-14 20:48:13 -0700

    creates form

--- routes/edit.js
+++ routes/edit.js
@@ -1,9 +1,14 @@
 const { compareSync } = require('bcrypt');
 const express = require('express');
+var methodOverride = require('method-override')
 const router = express.Router();
 const  db = require("../models");
 const strava = require('strava-v3');
 const axios = require('axios');  
+
+//middleware
+router.use(methodOverride('_method'))
+
 
 router.get('/', function(req, res) {
     console.log("~~~~~~~~~~~~~~~~~~~~~")
@@ -17,10 +22,10 @@ router.get('/', function(req, res) {
         headers: {
           "Authorization": `Bearer ${req.user.access_token}`
         }, 
-       name: 'this is a test'
+       name: req.body
       }).then(function(apiResponse) {
         // console.log('FIRST', apiResponse)
-        res.render('activities')
+        res.render('edit')
       })
     });
   
